7ddd510e01253e67b3aff90483418e30271899e1
Taciano da Hora <taciano.dev@outlook.com>
2023-11-30 16:36:24 -0300

    test: initial testing and application of design patterns

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,16 @@
 import { hash } from "bcryptjs";
 import { UsersRepository } from "@/repositories/users-repository";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
     name: string;
     email: string;
     password: string;
+}
+
+interface RegisterUseCaseResponse {
+    user: User
 }
 
 export class RegisterUseCase {
@@ -24,11 +29,15 @@ export class RegisterUseCase {
             throw new UserAlreadyExistsError();
         }
     
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name, 
             email, 
             password_hash
         });
+
+        return {
+            user
+        };
     }
 }
 
